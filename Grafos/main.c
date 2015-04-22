ba1ee7c190d075e544e69db6c71f4fe8f822fe8e
pedroabgmarques <pedroabgmarques@gmail.com>
2015-04-22 17:41:03 +0100

    Travessia em profundidade (Depth-First)

--- Grafos/main.c
+++ Grafos/main.c
@@ -6,19 +6,31 @@
 
 int grafo[LINHAS][COLUNAS];
 
+int visitado[LINHAS];
+
+void inicializarGrafo(){
+	for (int i = 0; i < LINHAS; i++){
+		for (int j = 0; j < COLUNAS; j++){
+			grafo[i][j] = -1;
+		}
+	}
+}
+
 void inserirAresta(int origem, int destino, int peso){
-	grafo[origem][destino] = peso;
+	if (origem >= 0 && origem < LINHAS && destino >= 0 && destino < COLUNAS){
+		grafo[origem][destino] = peso;
+	}
 }
 
 void removerAresta(int origem, int destino){
-	grafo[origem][destino] = 0;
+	grafo[origem][destino] = -1;
 	printf("Aresta %d -> %d removida.\n\n", origem, destino);
 }
 
 void listar(){
 	for (int i = 0; i < LINHAS; i++){
 		for (int j = 0; j < COLUNAS; j++){
-			if (grafo[i][j] != 0){
+			if (grafo[i][j] != -1){
 				printf("%d -> %d: %d\n", i, j, grafo[i][j]);
 			}
 		}
@@ -27,23 +39,50 @@ void listar(){
 }
 
 void sucessores(int vertice){
-	printf("Sucessores de %d:\n", vertice);
+	printf("\nSucessores de %d:\n", vertice);
 	for (int i = 0; i < COLUNAS; i++){
-		if (grafo[vertice][i] != 0){
+		if (grafo[vertice][i] != -1){
 			printf("%d -> %d : %d\n", vertice, i, grafo[vertice][i]);
 		}
 	}
 }
 
+void antecessores(int vertice){
+	printf("\nAntecessores de %d:\n", vertice);
+	if (vertice >= 0 && vertice < LINHAS){
+		for (int i = 0; i < LINHAS; i++){
+			if (grafo[i][vertice] != -1){
+				printf("%d -> %d : %d\n", i, vertice, grafo[i][vertice]);
+			}
+		}
+	}
+}
+
+void travessiaProfundidade(int vertice, int visitados[LINHAS]){
+	visitados[vertice] = 1;
+	printf("%d ", vertice);
+	for (int i = 0; i < LINHAS; i++){
+		if (grafo[vertice][i] != -1 && !visitados[i]){
+			travessiaProfundidade(i, visitados);
+		}
+	}
+}
+
+
 int main(){
 
-	inserirAresta(0, 2, 7);
-	inserirAresta(0, 3, 10);
-	inserirAresta(1, 1, 9);
-	inserirAresta(2, 0, 5);
-	inserirAresta(2, 3, 4);
-	inserirAresta(3, 3, 13);
-	inserirAresta(3, 1, 13);
+	int visitados[LINHAS];
+	for (int i = 0; i < LINHAS; i++){
+		visitados[i] = 0;
+	}
+
+	inicializarGrafo();
+
+	inserirAresta(0, 1, 5);
+	inserirAresta(0, 2, 10);
+	inserirAresta(1, 2, 7);
+	inserirAresta(2, 3, 2);
+
 
 	listar();
 
@@ -52,6 +91,12 @@ int main(){
 	listar();
 
 	sucessores(2);
+
+	antecessores(2);
+
+	printf("\nTravessia em profundidade: (Depth-First)\n");
+	travessiaProfundidade(0, visitados);
+	printf("\n");
 
 	return 1;
 }
